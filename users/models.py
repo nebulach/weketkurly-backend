91b20bc5ba95fbd030ec3924627629a683bc4104
ybear90 <chunggryu@gmail.com>
2020-02-28 16:22:47 +0900

    Address class in models.py and urls.py modified.

--- users/models.py
+++ users/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     account     = models.CharField(max_length=45, null=False, unique=True)
-    grade_id    = models.ForeignKey('Grade', models.SET_NULL, blank=True, null=True)
+    grade       = models.ForeignKey('Grade', models.SET_NULL, blank=True, null=True)
     password    = models.CharField(max_length=50)
     username    = models.CharField(max_length=45)
     email       = models.EmailField(max_length=100, null=False, unique=True)
     phone       = models.CharField(max_length=15)
-    gender_id   = models.ForeignKey('Gender', models.SET_NULL, blank=True, null=True)
+    gender      = models.ForeignKey('Gender', models.SET_NULL, blank=True, null=True)
     birthday    = models.CharField(max_length=15)
     created_at  = models.DateTimeField(auto_now_add=True)
     updated_at  = models.DateTimeField(auto_now=True)
@@ -33,7 +33,7 @@ class Grade(models.Model):
 class Address(models.Model):
     user            = models.ForeignKey('User', on_delete=models.CASCADE)
     address         = models.CharField(max_length=200)
-    capital_area    = models.BooleanField(default=False)
+    is_capital_area = models.BooleanField(default=False)
 
     class Meta:
         db_table = 'addresses'
