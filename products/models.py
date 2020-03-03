42e51f1f76b45b82d9194bd10383cc6029d6b420
anrunda9 <ljy6816@gmail.com>
2020-03-03 15:12:17 +0900

    카테고리 models.py

---
+++ products/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class MainCategory(models.Model):
+    name            = models.CharField(max_length = 50)
+    icon_black_url  = models.CharField(max_length = 2000)
+    icon_active_url = models.CharField(max_length = 2000)
+
+    class Meta : 
+        db_table : 'main_categories'
+
+
+class SubCategory(models.Model):
+    maincategory = models.ForeignKey('MainCategory', on_delete = models.CASCADE, related_name='subcategory')
+    name            = models.CharField(max_length = 50)
+    thumbnail_url   = models.CharField(max_length = 2000, blank = True, null = True)
+
+    class Meta :
+        db_table : 'sub_categories'
