2130ea0ba856afe7fcb577ffe189f2d0805aceb9
anrunda9 <ljy6816@gmail.com>
2020-03-06 11:43:38 +0900

    modelspy,viewspy 일단락

--- WeketKurly_backend/urls.py
+++ WeketKurly_backend/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('users', include('users.urls')),
-    path('orders', include('orders.urls'))
+    path('orders', include('orders.urls')),
+    path('products', include('products.urls'))
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from .models            import MainCategory, SubCategory
+
+from django.views       import View
+from django.http        import HttpResponse, JsonResponse 
+
+
+class CategoryView(View):
+    def get(self, request):
+        category_list = [
+            {       'main_category'         : main.name,
+                    'icon_black_url'        : main.icon_black_url,
+                    'icon_active_url'       : main.icon_active_url,
+                    'subcategory'           : [{
+                        'name'          : sub.name, 
+                        'thumbnail_url' : sub.thumbnail_url
+                    } for sub in SubCategory.objects.filter(maincategory = main.id).all()] 
+                    }
+            for main in MainCategory.objects.all()
+        ]
+        return JsonResponse({"data" : list(category_list)}, status = 200)
+
